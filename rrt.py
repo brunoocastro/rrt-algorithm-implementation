070b89309643fe163308771c664936c2d887f762
Tone <bruno.scastro2012@hotmail.com>
2024-04-04 14:18:16 -0300

    last_state

--- rrt.py
+++ rrt.py
@@ -1,22 +1,76 @@
+import math
 import random
+from typing import List
 import pygame
 import numpy as np
 
-# Inicialização do Pygame
-pygame.init()
-screen_size = 400
-screen = pygame.display.set_mode((screen_size, screen_size))
-pygame.display.set_caption("Mapa do Jogo")
+# Cores
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 
 class Map:
-    def __init__(self, size=4) -> None:
+    gameSprites = {"goal": None, "start": None, "nodes": [], "tiles": []}
+
+    def __init__(self, size=4, screen_size=400) -> None:
         self.size = size
         self.state = np.zeros((self.size, self.size), dtype=int)
+        self.screen_size = screen_size
+
+        # Inicialização do Pygame
+        # pygame.init()
+        # self.screen = pygame.display.set_mode((screen_size, screen_size))
+        # pygame.display.set_caption("Mapa do Jogo")
 
     def isFreePos(self, x, y):
-        print(self.state[x, y], self.state[x, y] == 0)
         return self.state[x, y] == 0
+
+    def transformTileToMapPos(self, tileX, tileY):
+        cell_size = self.screen_size // self.size
+        x = math.ceil(tileX / cell_size)
+        y = math.ceil(tileY / cell_size)
+        print("Transf", x, y)
+        return (y - 1, x - 1)
+
+    def render(self):
+        # print(self.state)
+
+        return self.state
+        # Desenha o mapa
+        cell_size = self.screen_size // self.size
+        for row in range(self.size):
+            for col in range(self.size):
+                tileBorder = pygame.Rect(
+                    col * cell_size, row * cell_size, cell_size, cell_size
+                )
+                list_of_nodes = [
+                    node for node in rrt.getNodes() if node.getPos() == (row, col)
+                ]
+
+                self.gameSprites["tiles"].append(tileBorder)
+                pygame.draw.rect(self.screen, BLACK, tileBorder, 1)  # Adiciona borda
+                if (row, col) == startPoint:
+                    self.gameSprites["start"] = tileBorder
+                    pygame.draw.rect(self.screen, BLUE, tileBorder)
+                elif (row, col) == goalPoint:
+                    self.gameSprites["goal"] = tileBorder
+                    pygame.draw.rect(self.screen, GREEN, tileBorder)
+                elif not self.isFreePos(row, col):
+                    pygame.draw.rect(self.screen, BLACK, tileBorder)
+                elif len(list_of_nodes) > 0:
+                    print("Entrou", list_of_nodes)
+                    node_tile = pygame.draw.circle(
+                        self.screen,
+                        RED,
+                        [row + cell_size / 2, col + cell_size / 2],
+                        cell_size / 3,
+                    )
+                    self.gameSprites["nodes"].append(node_tile)
+                else:
+                    pygame.draw.rect(self.screen, WHITE, tileBorder)
 
 
 class TreeNode:
@@ -37,73 +91,92 @@ class TreeNode:
         self.children.append(child)
 
 
+class RRTAlgorithm:
+    _nodeList: List[TreeNode] = []
+    reachedGoal = False
+    goalRange = 2
+
+    def __init__(self, map: Map, start, goal) -> None:
+        self.map = map
+        self.startPoint = start
+        self.goalPoint = goal
+
+    def addNode(self, x, y):
+        isFree = self.map.isFreePos(x - 1, y - 1)
+
+        if not isFree:
+            print(f"Posição ({x},{y}) não está livre!")
+            return
+
+        parent = self._nodeList[-1] if len(self._nodeList) > 0 else None
+
+        newNode = TreeNode(x, y, parent)
+        self._nodeList.append(newNode)
+
+    def getNodes(self):
+        return self._nodeList
+
+
+# Configurações do mapa
+screen_size = 400
+map = Map(6)
+startPoint = (0, 0)
+goalPoint = (3, 3)
+
+
 def getRandomPos():
-    randomX = random.randint(0, map.size)
-    randomY = random.randint(0, map.size)
+    randomX = random.randint(0, map.size - 1)
+    randomY = random.randint(0, map.size - 1)
 
     return (randomX, randomY)
 
 
-class RRTAlgorithm:
-    def __init__(self, start, goal) -> None:
-        pass
-
-
-# Configurações do mapa
-map = Map(6)
-startPoint = (0, 0)
-goalPoint = (3, 3)  # Note que o índice é 3, pois o tamanho do mapa é 4x4
-
-# Cores
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-
-nodeList = [TreeNode(*startPoint)]
+rrt = RRTAlgorithm(map, startPoint, goalPoint)
 
 # Loop principal
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # for event in pygame.event.get():
+    #     if event.type == pygame.QUIT:
+    #         running = False
 
-    # Desenha o mapa
-    cell_size = screen_size // map.size
-    for row in range(map.size):
-        for col in range(map.size):
-            rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
-            pygame.draw.rect(screen, BLACK, rect, 10)  # Adiciona borda
-            if (row, col) == startPoint:
-                pygame.draw.rect(screen, BLUE, rect)
-            elif (row, col) == goalPoint:
-                pygame.draw.rect(screen, GREEN, rect)
-            elif not map.isFreePos(row, col):
-                pygame.draw.rect(screen, BLACK, rect)
-            elif len([node.getPos() == (row, col) for node in nodeList]) > 0:
-                pygame.draw.circle(
-                    screen,
-                    RED,
-                    [row + cell_size / 2, col + cell_size / 2],
-                    cell_size / 3,
-                )
-            else:
-                pygame.draw.rect(screen, WHITE, rect)
+    # # Roda o algoritmo RRT
+    # try:
+    #     new_random_pos = getRandomPos()
+    #     print("Nova posição aleatória", new_random_pos)
+    #     rrt.addNode(*new_random_pos)
+    # except Exception as error:
+    #     print(error)
 
-    try:
-        new_random_pos = getRandomPos()
-        print(new_random_pos)
-        if map.isFreePos(*new_random_pos):
-            newNode = TreeNode(*new_random_pos)
-            nodeList.append(newNode)
-        else:
-            print("Posição ocupada!")
-    except Exception as error:
-        print(error)
+    mapState = map.render()
+    randomPos = getRandomPos()
+    rrt.addNode(*randomPos)
+    currentNodes = rrt.getNodes()
 
-    pygame.display.flip()
+    nodesPos = [node.getPos() for node in currentNodes]
+
+    # Print pretty map with:
+    # 0 - Free
+    # 1 - Obstacle
+    # 2 - Start
+    # 3 - Goal
+    # 4 - Nodes
+    printMap = mapState
+    printMap[startPoint] = 2
+    printMap[goalPoint] = 3
+    printMap[nodesPos] = 4
+    print(printMap)
+
+    # pygame.display.flip()
+    # lastEvent = pygame.event.wait(0)
+    # print(lastEvent)
+
+    # if lastEvent.type == pygame.MOUSEBUTTONUP:
+    #     pos = pygame.mouse.get_pos()
+    #     clicked_sprites = [s for s in map.gameSprites["tiles"] if s.collidepoint(pos)]
+    #     print("Clicked", clicked_sprites, map.transformTileToMapPos(*pos))
+    #     rrt.addNode(*map.transformTileToMapPos(*pos))
+    #     print("Nodes", rrt.getNodes())
 
 
 pygame.quit()
